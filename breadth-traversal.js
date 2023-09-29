62e1b1d7aa4b1fbbf11e3137773951f774e89373
zacharybaca <zachary.baca@msn.com>
2023-09-28 22:51:39 -0500

    added

---
+++ breadth-traversal.js
@@ -0,0 +1,6 @@
+function breathFirst() {
+    // Put the starting node in a queue.
+    // While the queue is not empty, repeat steps 3-4.
+    // Dequeue a node and print it.
+    // Put all of the node's children in the back of the queue.
+}
---
+++ depth-traversal.js
@@ -0,0 +1,6 @@
+function depthFirst() {
+    // Put the starting node on a stack.
+    // While the stack is not empty, repeat steps 3-4.
+    // Pop a node and print it.
+    // Put all of the node's children on the top of the stack.
+}
