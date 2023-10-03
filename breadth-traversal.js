2be759888f68a03e9d8c5127a7004a137cfbfee4
zacharybaca <zachary.baca@msn.com>
2023-10-03 13:00:46 -0500

    complete

--- breadth-traversal.js
+++ breadth-traversal.js
@@ -1,6 +1,75 @@
-function breadthFirst() {
-    // Put the starting node in a queue.
-    // While the queue is not empty, repeat steps 3-4.
-    // Dequeue a node and print it.
-    // Put all of the node's children in the back of the queue.
+//Create Tree Node
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
+
+function breadthFirst(root) {
+    if (!root) return [];
+    // Put the starting node in a queue.
+    let queue = [root];
+    // While the queue is not empty, repeat steps 3-4.
+    while (queue.length) {
+        // Dequeue a node and print it.
+        let curr = queue.shift();
+        console.log(curr.value);
+
+        // Put all of the node's children in the back of the queue.
+        if (curr.left) queue.push(curr.left);
+        if (curr.right) queue.push(curr.right);
+    }
+
+}
+
+// Create Nodes
+let rootNode = new TreeNode(1);
+rootNode.left = new TreeNode(2);
+rootNode.right = new TreeNode(3);
+rootNode.left.right = new TreeNode(4);
+rootNode.right.left = new TreeNode(5);
+rootNode.left.right.right = new TreeNode(6);
+rootNode.right.left.left = new TreeNode(7);
+
+//breadthFirst(rootNode);
+
+
+// Create Adj List
+let adjList = {
+    1: [2, 5],
+    2: [1, 3, 5],
+    3: [2, 4],
+    4: [3, 5, 6],
+    5: [1, 2, 4],
+    6: [4]
+}
+
+
+function depthFirstGraph(list, start) {
+    // Create a queue and push the starting node
+    const queue = [start];
+    // Create a set to store visited nodes, and add the starting node
+    const visited = new Set();
+    visited.add(start);
+    // While the queue is not empty, repeat steps 4-6
+    while (queue.length) {
+        // Pop the node on the top of the queue.
+        const current = queue.shift();
+        // DO THE THING THAT YOU NEED TO DO FOR THE POPPED NODE
+        //     For example, add it to a running total, print it, or save it in an array
+        console.log(current);
+        visited.add(current);
+        // For each unvisited neighbor, add it to the visited nodes and to the top of the queue.
+        for (const neighbor of list[current]) {
+            if (!visited.has(neighbor)) {
+                queue.push(neighbor);
+                visited.add(neighbor);
+            }
+        }
+    }
+
+}
+
+depthFirstGraph(adjList, 1);
--- depth-traversal.js
+++ depth-traversal.js
@@ -32,7 +32,7 @@ rootNode.right.left = new TreeNode(5);
 rootNode.left.right.right = new TreeNode(6);
 rootNode.right.left.left = new TreeNode(7);
 
-depthFirstTree(rootNode);
+//depthFirstTree(rootNode);
 
 // Create Adj List
 let adjList = {
@@ -45,12 +45,29 @@ let adjList = {
 }
 
 
-function depthFirstGraph(vertex) {
+function depthFirstGraph(list, start) {
     // Create a stack and push the starting node
+    const stack = [start];
     // Create a set to store visited nodes, and add the starting node
+    const visited = new Set();
+    visited.add(start);
     // While the stack is not empty, repeat steps 4-6
-    // Pop the node on the top of the stack.
-    // DO THE THING THAT YOU NEED TO DO FOR THE POPPED NODE
-    //     For example, add it to a running total, print it, or save it in an array
-    // For each unvisited neighbor, add it to the visited nodes and to the top of the stack.
+    while (stack.length) {
+        // Pop the node on the top of the stack.
+        const current = stack.pop();
+        // DO THE THING THAT YOU NEED TO DO FOR THE POPPED NODE
+        //     For example, add it to a running total, print it, or save it in an array
+        console.log(current);
+        visited.add(current);
+        // For each unvisited neighbor, add it to the visited nodes and to the top of the stack.
+        for (const neighbor of list[current]) {
+            if (!visited.has(neighbor)) {
+                stack.push(neighbor);
+                visited.add(neighbor);
+            }
+        }
+    }
+
 }
+
+depthFirstGraph(adjList, 1);
